ee711eab92b7ec4b71e7fe6e533d90588d502e24
chr1s <gongqi@xiaoluyy.com>
2020-09-21 18:38:12 +0800

    directed graph

--- algorithms/src/main/java/io/chr1s/graph/SCC.java
+++ algorithms/src/main/java/io/chr1s/graph/SCC.java
@@ -1,38 +1,12 @@
 package io.chr1s.graph;
 
-import edu.princeton.cs.algs4.Bag;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class SCC {
-
-    private boolean[] onStack;
-
-    private List<Bag<Integer>> map;
+public interface SCC {
 
     /**
-     * 预处理构造函数
-     * @param G
+     * 图中的强连通分量的总数
+     * @return
      */
-    public SCC(Digraph G) {
-        this.onStack = new boolean[G.V()];
-        this.map = new ArrayList<>();
-        for (int v = 0; v < G.V(); v++) {
-            onStack[v] = true;
-            dfs(G, v);
-        }
-    }
-
-    private void dfs(Digraph G, int v) {
-        for (int w : G.adj(v)) {
-            if (!onStack[w]) {
-                onStack[w] = true;
-                dfs(G, w);
-                onStack[w] = false;
-            }
-        }
-    }
+    int count();
 
     /**
      * 顶点v和w是否强连通
@@ -40,26 +14,12 @@ public class SCC {
      * @param w
      * @return
      */
-    public boolean stronglyConnected(int v, int w) {
-        return false;
-    }
-
-    /**
-     * 图中的强连通分量的总数
-     * @return
-     */
-    public int count() {
-        return 0;
-    }
+    boolean stronglyConnected(int v, int w);
 
     /**
      * v 所在的强连通分量的表标识符，0~count() - 1
      * @param v
      * @return
      */
-    public int id(int v) {
-        return 0;
-    }
-
-
+    int id(int v);
 }
